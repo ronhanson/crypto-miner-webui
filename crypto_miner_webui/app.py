95ff8d3ec684a9f67cfeffec068d1605b1a29ace
Ronan Delacroix <ronan.delacroix@gmail.com>
2018-02-20 16:59:12 +0100

    Changed wallet from electromine to spacepools by default and made that configurable. Added 2 graphs based on data from spacepools. Cosmetic changes. Minor improvements.

--- crypto_miner_webui/app.py
+++ crypto_miner_webui/app.py
@@ -7,6 +7,7 @@ Crypto Mining Web UI
 :author: Ronan Delacroix
 """
 import os
+import sys
 from flask import Flask, render_template
 from whitenoise import WhiteNoise
 from . import metrics
@@ -30,13 +31,19 @@ def index():
     """
     Main index page
     """
-    electromine_wallet_address = os.environ.get("ELECTROMINE_WALLET", None)
-    if not electromine_wallet_address:
-        raise Exception("ENV Variable ELECTROMINE_WALLET not set")
+    wallet_id = os.environ.get("WALLET_ID", None)
+    if not wallet_id:
+        raise Exception("ENV Variable WALLET_ID not set")
+
+    wallet_api_url = os.environ.get("WALLET_API_URL", None)
+    if not wallet_api_url:
+        print('Warning - ENV Variable WALLET_API_URL not set, using "https://etn.spacepools.org/api/stats_address" by default.')
+        wallet_api_url = 'https://etn.spacepools.org/api/stats_address'
 
     tmpl_dict = {
         'app_name': app_name,
-        "wallet_address": electromine_wallet_address,
+        "wallet_id": wallet_id,
+        "wallet_api_url": wallet_api_url,
         "grafana_host": os.environ.get("GRAFANA_SERVICE_HOST", None),
         "grafana_port": os.environ.get("GRAFANA_SERVICE_PORT", 80),
         "grafana_graph_id": os.environ.get("GRAFANA_GRAPH_ID", None)
--- crypto_miner_webui/static/js/wallet.js
+++ crypto_miner_webui/static/js/wallet.js
@@ -5,75 +5,153 @@
 
 $(document).ready(function() {
 
+
+    function graph_transaction(transactions_graph_x_dates, transactions_graph_y_values) {
+        var last_val = 0;
+        var transactions_graph_y_stacked_values = _.map(transactions_graph_y_values, function (val) {
+            last_val = last_val + val;
+            return last_val;
+        });
+        var transactions_trace1 = {
+            x: transactions_graph_x_dates,
+            y: transactions_graph_y_stacked_values,
+            name: 'Balance over time (ETN)',
+            fill: 'tonexty',
+            type: 'scatter'
+        };
+
+        var marker_size_factor = 30/_.max(transactions_graph_y_values);
+        var transaction_trace2 = {
+            x: transactions_graph_x_dates,
+            y: transactions_graph_y_values,
+            marker: {
+                color: 'rgb(188, 208, 220)',
+                size: _.map(transactions_graph_y_values, function (d) {return d*marker_size_factor;}),
+            },
+            name: 'Payments (ETN)',
+            yaxis: 'y2',
+            mode: 'markers',
+            type: 'scatter'
+        };
+        var layout = {
+            title: 'Mining pool Payments',
+	        legend: {
+                orientation: "h",
+                x: 0.35
+            },
+            margin: {
+                t: 50,
+                b: 10
+            },
+            yaxis: {
+                title: 'Overall Balance (ETN)',
+                hoverformat: '.1f',
+                ticksuffix: ' ETN'
+            },
+            yaxis2: {
+                title: 'Payments (ETN)',
+                titlefont: {color: 'rgb(188, 208, 220)'},
+                tickfont: {color: 'rgb(188, 208, 220)'},
+                hoverformat: '.1f',
+                ticksuffix: ' ETN',
+                overlaying: 'y',
+                rangemode:'tozero',
+                side: 'right'
+            }
+        };
+        Plotly.newPlot('transactions_graph', [transactions_trace1, transaction_trace2], layout);
+    }
+
+    function graph_hash(data) {
+        var dates = _.map(data, _.first);
+        dates = _.map(dates, function(d) {return moment(d * 1000).toISOString();});
+        var rate = _.map(data, function(d) {return d[1];});
+        var hash_trace = {
+            x: dates,
+            y: rate,
+            name: 'Balance over time (ETN)',
+            fill: 'tonexty',
+            type: 'scatter'
+        };
+        var layout = {
+            title: 'Mining pool Hash Rate',
+            yaxis: {
+                title: 'Hashrate (H/s)',
+                ticksuffix: ' H/s'
+            },
+            showlegend: false
+        };
+        Plotly.newPlot('hash_graph', [hash_trace], layout);
+    }
+
+    function graph_payment(data) {
+        var dates = _.map(data, _.first);
+        dates = _.map(dates, function(d) {return moment(d * 1000).toISOString();});
+        var rate = _.map(data, function(d) {return d[1];});
+        var hash_trace = {
+            x: dates,
+            y: rate,
+            name: 'Payments over time (ETN)',
+            fill: 'tonexty',
+            type: 'scatter'
+        };
+        var layout = {
+            title: 'Payments',
+            yaxis: {
+                title: 'Value (ETN)',
+                ticksuffix: ' ETN'
+            },
+            showlegend: false
+        };
+        Plotly.newPlot('payment_graph', [hash_trace], layout);
+    }
+
+    function process_stats(vm, data) {
+
+        vm.stats = data.stats;
+        vm.stats.lastShare = moment(vm.stats.lastShare*1000).calendar();
+
+        var transactions_graph_x_dates = [];
+        var transactions_graph_y_values = [];
+        var transactions = [];
+        var payments = data.payments;
+
+        for (var i = 0; i < payments.length; i += 2) {
+            var p = payments[i];
+            var parts = p.split(':');
+            var payment = {
+                time: moment(payments[i + 1] * 1000).calendar(),
+                hash: parts[0],
+                amount: (parts[1] / 100.0).toFixed(2) + ' ETN',
+                fee: (parts[2] / 100.0).toFixed(2) + ' ETN',
+                mixin: parts[3]
+            };
+            transactions.push(payment);
+            transactions_graph_x_dates.push(moment(payments[i + 1] * 1000).toISOString())
+            transactions_graph_y_values.push(parts[1] / 100.0)
+        }
+        vm.transactions = transactions;
+        vm.message = 'Wallet loaded';
+
+        transactions_graph_x_dates = transactions_graph_x_dates.reverse();
+        transactions_graph_y_values = transactions_graph_y_values.reverse();
+
+        graph_transaction(transactions_graph_x_dates, transactions_graph_y_values);
+
+        if (data.charts) {
+            $('#hash_graph').removeClass('hidden');
+            $('#payment_graph').removeClass('hidden');
+            graph_hash(data.charts.hashrate);
+            graph_payment(data.charts.payments);
+        }
+    }
+
     function refresh_wallet() {
         this.message = 'Loading...';
         this.loading = true;
         var vm = this;
-        axios.get('https://api.electromine.fr/stats_address?address='+WALLET_ADDRESS).then(function (response) {
-            vm.stats = response.data.stats;
-            vm.stats.lastShare = moment(vm.stats.lastShare*1000).calendar()
-
-            var graph_x_dates = [];
-            var graph_y_values = [];
-            /*var graph_y_top_value =0;*/
-            var transactions = [];
-            var payments = response.data.payments;
-
-            for (var i = 0; i < payments.length; i += 2) {
-                var p = payments[i];
-                var parts = p.split(':');
-                var payment = {
-                    time: moment(payments[i + 1]*1000).calendar(),
-                    hash: parts[0],
-                    amount: (parts[1]/100.0).toFixed(2) + ' ETN',
-                    fee: (parts[2]/100.0).toFixed(2) + ' ETN',
-                    mixin: parts[3]
-                };
-                transactions.push(payment);
-                graph_x_dates.push(moment(payments[i + 1]*1000).toISOString())
-                /*graph_y_top_value = graph_y_top_value+(parts[1]/100.0)*/
-                graph_y_values.push(parts[1]/100.0)
-            }
-            vm.transactions = transactions;
-            vm.message = 'Wallet loaded';
-
-            graph_x_dates = graph_x_dates.reverse();
-            graph_y_values = graph_y_values.reverse();
-            var last_val = 0;
-            var graph_y_stacked_values = _.map(graph_y_values, function(val) {
-                last_val = last_val + val;
-                return last_val;
-            });
-            var graph_data = [{
-                x: graph_x_dates,
-                y: graph_y_stacked_values,
-                name: 'Balance over time (ETN)',
-                fill: 'tonexty',
-                type: 'scatter'
-            },{
-                x: graph_x_dates,
-                y: graph_y_values,
-                marker: {
-                    color:'rgb(188, 208, 220)',
-                    size:graph_y_values,
-                },
-                name: 'Payments (ETN)',
-                yaxis: 'y2',
-                mode: 'markers',
-                type: 'scatter'
-            }];
-            var layout = {
-                title: 'Eletromine Payments',
-                yaxis: {title: 'Overall Balance (ETN)'},
-                yaxis2: {
-                    title: 'Payments (ETN)',
-                    titlefont: {color: 'rgb(188, 208, 220)'},
-                    tickfont: {color: 'rgb(188, 208, 220)'},
-                    overlaying: 'y',
-                    side: 'right'
-                }
-            };
-            Plotly.newPlot('transactions_graph', graph_data, layout);
+        axios.get(WALLET_API_URL+'?address='+WALLET_ID).then(function (response) {
+            process_stats(vm, response.data);
             $.notify(vm.message, 'info');
         }).catch(function(error) {
             vm.message = 'Error ' + error;
@@ -86,6 +164,7 @@ $(document).ready(function() {
             var electroneum = _.where(response.data, {id: 'electroneum'})[0];
             vm.price.usd = electroneum.price_usd;
             vm.price.percent_change_24h = electroneum.percent_change_24h;
+            vm.price.percent_change_7d = electroneum.percent_change_7d;
         }).catch(function(error) {
             vm.message = vm.message + 'Error ' + error;
             $.notify(vm.message, {className:'error', autoHideDelay: 10000});
